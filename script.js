6aea31b1c96209a30cd1dd2a14543652cd581e01
Rahul Kumar <thenamerahulkr@gmail.com>
2023-12-12 19:45:07 +0530

    first commit in js

---
+++ script.js
@@ -0,0 +1,17 @@
+let marks_student1 = 80;
+let marks_student2 = 100;
+let marks_student3 = 70;
+let marks_student4 = 80;
+let marks_student5 = 90;
+
+// create array
+let heroes = ["rahul","kumar","kavya","singh"]
+let marks = [91,92,93,94,95]
+console.log(marks)
+console.log(heroes)
+//  array length property array.lenght=>number of element in array
+console.log(heroes.length)
+
+// type of marks and heroes
+ console.log(typeof(heroes))
+//   array is key value pair but key ke jagah index use krte hain
